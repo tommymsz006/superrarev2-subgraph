7375f1fdcf8121ab7b711da9924aff6a890eeb33
tommymsz006 <37748412+tommymsz006@users.noreply.github.com>
2020-05-04 23:38:25 +0800

    further fixes 1) refine object and array persistence, 2) tidy up entities and states, 3) use transaction hash as IDs for bid, sale and transfer

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,6 +10,7 @@ export function handleTokenURIUpdated(event: TokenURIUpdated): void {
   let artwork = Artwork.load(tokenId);
   if (artwork != null) {
     artwork.uri = event.params._uri;
+    artwork.save();
     log.debug("URI updated: {}, {}", [tokenId, artwork.uri]);
   }
 }
@@ -33,14 +34,19 @@ export function handleTransfer(event: TransferEvent): void {
     let artwork = Artwork.load(tokenIdStr);
     if (artwork != null) {
       if (event.params.to.toHex() != BIRTH_ADDRESS) {
-        let transfer = new Transfer(tokenIdStr + event.block.timestamp.toString());
+        let transfer = new Transfer(event.transaction.hash.toHex());
         transfer.from = _loadAccount(event.params.from);
         transfer.to = _loadAccount(event.params.to);
         transfer.timestamp = event.block.timestamp;
-        artwork.transfers.push(transfer.id);
+        transfer.save();
+
+        let transfers = artwork.transfers;
+        transfers.push(transfer.id);
+        artwork.transfers = transfers;
 
         artwork.owner = _loadAccount(event.params.to);
         artwork.timeLastTransferred = transfer.timestamp;
+        artwork.save();
 
         log.debug("handleTransfer(): Artwork tranferred - {}, {}, {}", [tokenIdStr, transfer.from, transfer.to]);
       } else {
@@ -62,19 +68,25 @@ export function handleSetSalePrice(event: SetSalePrice): void {
       let sale = Sale.load(artwork.currentSale);
       if (sale != null) {
         sale.price = event.params._amount;
+        sale.save();
         log.debug("handleSetSalePrice(): Set new sale price - {}, {}", [tokenIdStr, sale.price.toString()]);
       } else {
         log.error("handleSetSalePrice(): Current sale not found - {}", [tokenIdStr]);
       }
     } else {
-      let sale = new Sale(tokenIdStr + event.block.timestamp.toString());
+      let sale = new Sale(event.transaction.hash.toHex());
       sale.seller = artwork.owner;
       sale.price = event.params._amount;
       sale.timeRaised = event.block.timestamp;
       sale.isSold = false;
+      sale.save();
 
-      artwork.status = (artwork.status == 'Created') ? 'OnPrimarySale' : 'OnSecondarySale';
+      //artwork.status = (artwork.status == 'Created') ? 'OnPrimarySale' : 'OnSecondarySale';
+      let sales = artwork.sales;
+      sales.push(sale.id);
+      artwork.sales = sales;
       artwork.currentSale = sale.id;
+      artwork.save();
 
       log.debug("handleSetSalePrice(): On sale - {}, {}", [tokenIdStr, sale.price.toString()]);
     }
@@ -93,6 +105,7 @@ export function handleSold(event: Sold): void {
       sale.isSold = true;
       sale.buyer = _loadAccount(event.params._buyer);
       sale.timeSold = event.block.timestamp;
+      sale.save();
     } else {
       log.error("handleSold(): Sale not found - {}", [artwork.currentSale]);
     }
@@ -102,6 +115,7 @@ export function handleSold(event: Sold): void {
     artwork.currentSale = null;
     artwork.lastTransferPrice = event.params._amount;
     artwork.timeLastTransferred = event.block.timestamp;
+    artwork.save();
 
     log.debug("handleSold(): Artwork sold - {}, {}, {}", [tokenIdStr, event.params._buyer.toHex(), artwork.lastTransferPrice.toString()]);
   } else {
@@ -114,14 +128,18 @@ export function handleBid(event: BidEvent): void {
   let artwork = Artwork.load(tokenIdStr);
 
   if (artwork != null) {
-    let bid = new Bid(tokenIdStr + event.block.timestamp.toString() + event.params._bidder.toHex());
+    let bid = new Bid(event.transaction.hash.toHex());
     bid.bidder = _loadAccount(event.params._bidder);
     bid.price = event.params._amount;
     bid.timeRaised = event.block.timestamp;
     bid.status = 'Open';
+    bid.save();
 
-    artwork.bids.push(bid.id);
+    let bids = artwork.bids;
+    bids.push(bid.id);
+    artwork.bids = bids;
     artwork.currentBid = bid.id;
+    artwork.save();
 
     log.debug("handlBid(): Bid raised - {}, {}, {}", [tokenIdStr, event.block.timestamp.toString(), event.params._bidder.toHex()]);
   } else {
@@ -138,6 +156,7 @@ export function handleCancelBid(event: CancelBid): void {
     if (bid != null) {
       bid.status = 'Cancelled';
       bid.timeCancelled = event.block.timestamp;
+      bid.save();
 
       log.debug("handleCancelBid(): Bid cancelled: {}", [bid.id]);
     } else {
@@ -158,17 +177,19 @@ export function handleAcceptBid(event: AcceptBid): void {
       bid.status = 'Accepted';
       bid.acceptedBy = _loadAccount(event.params._seller);
       bid.timeAccepted = event.block.timestamp;
+      bid.save();
 
       log.debug("handleAcceptBid(): Bid accepted: {}, {}", [bid.id, bid.acceptedBy]);
     } else {
       log.error("handleAcceptBid(): Accepted bid not found - {}", [artwork.currentBid]);
     }
 
-    artwork.status = 'Sold';
+    //artwork.status = 'Sold';
     artwork.currentBid = null;
     artwork.currentSale = null;
     artwork.lastTransferPrice = event.params._amount;
     artwork.timeLastTransferred = event.block.timestamp;
+    artwork.save();
   } else {
     log.error("handleAcceptBid(): Artwork not found - {}", [tokenIdStr]);
   }
